b2570b02fb79bb07a0063b51c48827618c7dc65f
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-20 19:06:59 -0400

    Refactor: Add game card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { GameCardComponent } from './components/game-card/game-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameCardComponent
   ],
   imports: [
     BrowserModule,
