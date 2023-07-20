366bb5f7b0ffb949d0745412dfe2fbdf2cffac91
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-20 18:33:32 -0400

    New: Implement a custom infinite scroll to list games

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { GameDataService } from './services/game-data.service';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   
-  games: any;
+  gamesAll: any;
+  gamesPortion: any;
+  pageSize: number = 10;
 
   constructor(private gameDataService: GameDataService){}
   
@@ -18,7 +20,29 @@ export class AppComponent {
 
   getGames() {
     this.gameDataService.getGames().subscribe((games) =>{
-      this.games = games;
+      this.gamesAll = games;
+      this.gamesPortion = this.gamesAll.slice(0, this.pageSize);
     });
+  }
+
+  @HostListener("window:scroll", ['$event'])
+  doSomethingOnScroll(){
+    const visibleHeight = window.innerHeight;
+    const totalHeight = document.documentElement.scrollHeight;
+    const scrollY = window.scrollY;
+
+    // Check if the user has reached the bottom of the page
+    if (scrollY + visibleHeight >= totalHeight) {
+      // Calculate the current page number (add +1 to account for 0-based indexing)
+      let pageNumber = Math.floor(scrollY/visibleHeight)+1;
+
+      let index = pageNumber*this.pageSize;
+      let nextPageGames = this.gamesAll.slice(index, index + this.pageSize);
+
+      // Check if there are more games to load
+      if (nextPageGames.length > 0) {
+        this.gamesPortion = this.gamesPortion.concat(nextPageGames);
+      }
+    }
   }
 }
