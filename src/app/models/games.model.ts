983876202bd95c66ed88b54e3e828aabf8602a77
Luis Leguizamón <lleguizamonl@gmail.com>
2023-07-24 21:09:44 -0400

    Adjust: Basic UI for game detail page

--- src/app/models/games.model.ts
+++ src/app/models/games.model.ts
@@ -1,8 +1,10 @@
 export interface Game {
     id: Number;
+    developer: String,
     title: String;
     platform: String;
     genre: String;
+    publisher: String;
     short_description: String;
     thumbnail: String;
 }
--- src/app/pages/game-detail/game-detail.component.ts
+++ src/app/pages/game-detail/game-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Game } from 'src/app/models/games.model';
 
 @Component({
   selector: 'app-game-detail',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-detail.component.css']
 })
 export class GameDetailComponent {
-
+  game: Game = {
+    id: 1,
+    developer: 'Blizzard',
+    title: 'Game 1',
+    platform: 'Web',
+    genre: 'Shooter',
+    publisher: 'Activision Blizzard',
+    short_description: 'A hero-focused first-person team shooter from Blizzard Entertainment.',
+    thumbnail: 'https://www.freetogame.com/g/540/thumbnail.jpg'
+  };
 }
